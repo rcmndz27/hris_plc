afc8f463d1424a75761be4cf97e8459a04d84dee
Ric Mendoza <rick.mendoza27@gmail.com>
2022-02-23 17:23:32 +0800

    fix db

--- config/db.php
+++ config/db.php
@@ -1,7 +1,7 @@
 <?php
 
             $dbstringsL = "sqlsrv:Server=192.168.201.8;Database=hrissys_test";
-            $connL = new PDO($dbstringsL, "mgr", "mgr");
+            $connL = new PDO($dbstringsL, "mgr", "P@55w0rd456");
             $connL->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             
 
@@ -9,7 +9,7 @@
             {
                 $dbstrings = "sqlsrv:Server=192.168.201.8;Database=hrissys_test";
                 
-                $dbConnection = new PDO($dbstrings, "mgr", "mgr"); 
+                $dbConnection = new PDO($dbstrings, "mgr", "P@55w0rd456"); 
 
                 $dbConnection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
                 return $dbConnection;
