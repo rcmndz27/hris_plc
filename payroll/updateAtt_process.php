d9016c38bb24d0991ccb9d117601bef8616a8d9a
Ric Mendoza <rick.mendoza27@gmail.com>
2022-04-26 10:19:14 +0800

    fix payrolsssssl

--- payroll/updateAtt_process.php
+++ payroll/updateAtt_process.php
@@ -7,7 +7,8 @@
     $badge_no = $_POST["badge_no"];
     $tot_days_absent = $_POST["tot_days_absent"];
     $tot_days_work = $_POST["tot_days_work"];   
-    $tot_lates = $_POST["tot_lates"];   
+    $tot_lates = $_POST["tot_lates"];  
+    $total_undertime = $_POST["total_undertime"];    
     $tot_overtime_reg = $_POST["tot_overtime_reg"];
     $night_differential = $_POST["night_differential"];
     $night_differential_ot = $_POST["night_differential_ot"];
@@ -35,7 +36,7 @@
     if ($action == 1)
     {
 
-UpdateAtt($badge_no,$tot_days_absent,$tot_days_work,$tot_lates,$tot_overtime_reg,$night_differential,$night_differential_ot,$tot_regholiday,$tot_overtime_regholiday,$tot_regholiday_nightdiff,$tot_overtime_regholiday_nightdiff,$tot_spholiday,$tot_overtime_spholiday,$tot_spholiday_nightdiff,$tot_overtime_spholiday_nightdiff,$tot_rest,$tot_overtime_rest,$night_differential_rest,$night_differential_ot_rest,$tot_overtime_rest_regholiday,$night_differential_rest_regholiday,$tot_overtime_night_diff_rest_regholiday,$tot_overtime_sprestholiday,$tot_sprestholiday_nightdiff,$tot_overtime_sprestholiday_nightdiff,$sick_leave,$vacation_leave);
+UpdateAtt($badge_no,$tot_days_absent,$tot_days_work,$tot_lates,$total_undertime,$tot_overtime_reg,$night_differential,$night_differential_ot,$tot_regholiday,$tot_overtime_regholiday,$tot_regholiday_nightdiff,$tot_overtime_regholiday_nightdiff,$tot_spholiday,$tot_overtime_spholiday,$tot_spholiday_nightdiff,$tot_overtime_spholiday_nightdiff,$tot_rest,$tot_overtime_rest,$night_differential_rest,$night_differential_ot_rest,$tot_overtime_rest_regholiday,$night_differential_rest_regholiday,$tot_overtime_night_diff_rest_regholiday,$tot_overtime_sprestholiday,$tot_sprestholiday_nightdiff,$tot_overtime_sprestholiday_nightdiff,$sick_leave,$vacation_leave);
     }
 
 ?>
