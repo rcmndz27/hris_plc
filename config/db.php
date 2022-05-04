00dff9518b618f3f9923c0ec004bb46d688918da
Ric Mendoza <rick.mendoza27@gmail.com>
2022-05-04 09:59:05 +0800

    add new generate attendance add db

--- config/db.php
+++ config/db.php
@@ -1,16 +1,16 @@
 <?php
 
-            $dbstringsL = "sqlsrv:Server=SYSDEV-RMENDOZA\SQL2019;Database=hrissys_test";
+            $dbstringsL = "sqlsrv:Server=192.168.201.8;Database=hrissys_test";
             $connL = new PDO($dbstringsL, "mgr", "P@55w0rd456");
             $connL->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
  
 
             try
             {
-            $dbstrings = "sqlsrv:Server=SYSDEV-RMENDOZA\SQL2019;Database=hrissys_dev";             
+            $dbstrings = "sqlsrv:Server=192.168.201.8;Database=hrissys_dev";             
             $dbConnection = new PDO($dbstrings, "mgr", "P@55w0rd456"); 
 
-            $dbstringsLs = "sqlsrv:Server=SYSDEV-RMENDOZA\SQL2019;Database=biotime8";     
+            $dbstringsLs = "sqlsrv:Server=192.168.201.8;Database=biotime8";     
             $dbConnectionL = new PDO($dbstringsLs, "mgr", "P@55w0rd456"); 
       
 
