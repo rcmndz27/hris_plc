902f97c86a7815347ce27f29afcb2296d8e37bf4
Ric Mendoza <rick.mendoza27@gmail.com>
2022-06-27 18:02:42 +0800

    fix error

--- config/db.php
+++ config/db.php
@@ -2,17 +2,17 @@
             date_default_timezone_set('Asia/Manila'); 
 
             $dbstringsL = "sqlsrv:Server=192.168.201.8;Database=hrissys_test";
-            $connL = new PDO($dbstringsL, "mgr", "P@55w0rd456");
+            $connL = new PDO($dbstringsL, "sa", "P@55w0rd123");
             $connL->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
  
 
             try
             {
             $dbstrings = "sqlsrv:Server=192.168.201.8;Database=hrissys_dev";             
-            $dbConnection = new PDO($dbstrings, "mgr", "P@55w0rd456"); 
+            $dbConnection = new PDO($dbstrings, "sa", "P@55w0rd123"); 
 
             $dbstringsLs = "sqlsrv:Server=192.168.201.8;Database=biotime8";     
-            $dbConnectionL = new PDO($dbstringsLs, "mgr", "P@55w0rd456"); 
+            $dbConnectionL = new PDO($dbstringsLs, "sa", "P@55w0rd123"); 
             }
 
 
