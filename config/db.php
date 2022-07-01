335fe186b7805642d8a6b617dc6f21abb1889244
Ric Mendoza <rick.mendoza27@gmail.com>
2022-07-01 18:30:21 +0800

    updated 070122

--- config/db.php
+++ config/db.php
@@ -1,26 +1,26 @@
 <?php
             date_default_timezone_set('Asia/Manila'); 
 
-            $dbstringsL = "sqlsrv:Server=192.168.50.137;Database=hrissys_test";
-            $connL = new PDO($dbstringsL, "biotime", "Ob@nana2022");
+            $dbstringsL = "sqlsrv:Server=SYSDEV-RMENDOZA\SQL2019;Database=hrissys_test";
+            $connL = new PDO($dbstringsL, "mgr", "P@55w0rd456");
             $connL->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
  
 
             try
             {
-            $dbstrings = "sqlsrv:Server=192.168.50.137;Database=hrissys_dev";             
-            $dbConnection = new PDO($dbstrings, "biotime", "Ob@nana2022"); 
+            $dbstrings = "sqlsrv:Server=SYSDEV-RMENDOZA\SQL2019;Database=hrissys_dev";             
+            $dbConnection = new PDO($dbstrings, "mgr", "P@55w0rd456"); 
 
-            $dbstringsLs = "sqlsrv:Server=192.168.50.137;Database=biotime8";     
-            $dbConnectionL = new PDO($dbstringsLs, "biotime", "Ob@nana2022"); 
+            $dbstringsLs = "sqlsrv:Server=SYSDEV-RMENDOZA\SQL2019;Database=biotime8";     
+            $dbConnectionL = new PDO($dbstringsLs, "mgr", "P@55w0rd456"); 
             }
 
 
             catch (PDOException $e)
             {
-                die($e->getMesbiotimege());
+                die($e->getMesmgrge());
                 echo 'Connection failed: ' ;
-                // . $e->getMesbiotimege();
+                // . $e->getMesmgrge();
             }
         
     
---
+++ sysdown.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Obanana | Web Portal</title>
+    <noscript><h3>Please enable Javascript in order to use this form.</h3><meta HTTP-EQUIV='refresh' content=0; url='JavascriptNotEnabled.php'></noscript>
+    
+    <meta charset='utf-8'>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <meta name="robots" content="noindex">
+    <meta http-equiv='cache-control' content='no-cache'>
+    <meta http-equiv='expires' content='0'>
+<meta http-equiv='pragma' content='no-cache'>
+<link type='image/x-png' rel='icon' href='img/ob_icon.png'>
+<link rel="stylesheet" type="text/css" href="node_modules/bootstrap/dist/css/bootstrap.min.css">
+<link rel='stylesheet' href='css/login_caru.css'>
+<link rel='stylesheet' href='css/login.css'>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+      <!-- Google Fonts -->
+<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Dosis:300,400,500,,600,700,700i|Lato:300,300i,400,400i,700,700i" rel="stylesheet">
+<script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+<script type='text/javascript' src='https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js'></script>
+<script type='text/javascript' src="node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
+<script type="text/javascript" src='js/script.js'></script>
+</head>
+<body class="login-page" >
+<div id = "myDiv" style="display:none;" class="loader"></div>
+<!-- end of code -->
+<div class="login-box">
+      <!-- /.login-logo -->
+      <div class="card card-outline card-primary">
+        <div class="card-header text-center">
+            
+            <a href="#" class="h5">
+            <img class="mb-2 img-fluid mx-auto d-block newoblogo" src="img/obfinallogo.png" alt="">    
+            <b>Human Resource Information System</b> </a>
+        </div>
+        <div class="card-body">
+          <!-- <p class="login-box-msg">Sign in to start your session</p> -->
+
+          <h2 style="text-align:center;">SYSTEM DOWN !!!!! <br> 
+          PLEASE CONTACT THE ADMINISTRATOR.</h2>
+        </div>
+        <!-- /.card-body -->
+      </div>
+      <!-- /.card -->
+    </div>
+    <!-- /.login-box -->
