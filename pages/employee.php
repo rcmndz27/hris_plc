f22427c9d1e6a028c8d001bde1ff8c57a916c4d8
Ric Mendoza <rick.mendoza27@gmail.com>
2022-09-08 10:35:31 +0800

    update 090122

--- pages/employee.php
+++ pages/employee.php
@@ -609,7 +609,7 @@ $attid = (isset($spresult['attid'])) ? "'".$spresult['attid']."'" : '' ;
             <div class="card-body">
                 <div class="row no-gutters align-items-center">
                     <div class="col mr-2">
-                        <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">EMPLOYE HANDBOOK
+                        <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">EMPLOYEE HANDBOOK
                             <?php echo date("Y") ?> </div>
                             <div class="row no-gutters align-items-center">
                                 <a href="../uploads/COD_HANDBOOK.pdf" target="_blank">
