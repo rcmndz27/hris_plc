c83863ccd7c974f3ff4a8f8895b75c5e8f6c7310
Ric Mendoza <rick.mendoza27@gmail.com>
2022-09-21 17:18:39 +0800

    update 090122

--- _header.php
+++ _header.php
@@ -125,7 +125,7 @@ else
 <link type='text/css' rel='stylesheet' href="<?= constant('FONTAWESOME_CSS'); ?>">
 <link rel="stylesheet" href="../css/header.css">
 <link rel="stylesheet" href="../css/custom.css">
-<link rel="stylesheet" href="../css/style.css"/>
+<link rel="stylesheet" href="../css/styles.css"/>
 <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
 rel="stylesheet">
 <script type='text/javascript' src='../js/jquery.min.js'></script>
