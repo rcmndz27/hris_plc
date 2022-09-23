f09fcadfbaa3790d907966df531eeae881f5e675
Ric Mendoza <rick.mendoza27@gmail.com>
2022-09-23 10:39:10 +0800

    update 090122

--- controller/dtr.php
+++ controller/dtr.php
@@ -62,9 +62,9 @@
     
                         $timeIn = (isset($result['timein']) ? $this->GetTime($result['timein']): '');
                         $timeOut = (isset($result['timeout']) ? $this->GetTime($result['timeout']) : '');
-                        $late = ($result['late'] > 0) ? round($result['late']*60).'Min/s'  : 0);
-                        $undertime = ($result['undertime'] > 0) ? round($result['undertime']*60).'Min/s': 0);
-                        $overtime = ($result['overtime'] > 0) ? round($result['overtime'],2).'Hr/s': 0);
+                        $late = ($result['late'] > 0 ? round($result['late']*60).'Min/s'  : 0);
+                        $undertime = ($result['undertime'] > 0 ? round($result['undertime']*60).'Min/s': 0);
+                        $overtime = ($result['overtime'] > 0 ? round($result['overtime'],2).'Hr/s': 0);
     
                         echo    "<tr>".
                                 "<td>" . date('F d, Y', strtotime($result['punch_date'])) . "</td>".
